e1827c1a0e00027378b69dafc581ef8cbf8d36ad
nithya-1385 <184599296+nithya-1385@users.noreply.github.com>
2026-04-09 22:13:39 +0530

    Created Thymeleaf rendering for users page and fixed thymeleaf and supabase build dependencies

--- src/main/java/com/crms/placement/repository/TestUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crms.placement.repository;
-
-import com.crms.placement.entity.TestUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestUserRepository extends JpaRepository<TestUser, Long> {
-}
